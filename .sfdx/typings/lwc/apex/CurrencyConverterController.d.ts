af6314cbcb8a50b4e5383f318285900238886bf5
Rohan <rohan@IN-DL130152.local>
2026-08-18 18:43:00 +0530

    Initial commit

--- .sfdx/typings/lwc/apex/CurrencyConverterController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/CurrencyConverterController.convertCurrency" {
-  export default function convertCurrency(param: {fromCurrency: any, toCurrency: any, amount: any}): Promise<any>;
-}
--- .sfdx/typings/lwc/apex/LeadController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/LeadController.getLeadRecordImperatively" {
-  export default function getLeadRecordImperatively(): Promise<any>;
-}
